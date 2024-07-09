6772f359141ec365a9922461b3e0d20b5312d065
asharyver13 <asharyver13@gmail.com>
2024-07-09 08:11:04 +0000

    chore: change to basic posix command

--- idx-template.nix
+++ idx-template.nix
@@ -9,8 +9,16 @@
   services.docker.enable = true;
 
   bootstrap = ''
-    mkdir "$out"
-    # We can now use "npm"
-    npm init --yes my-boot-strap@latest "$out"
+    # Copy the folder containing the `idx-template` files to the final
+    # project folder for the new workspace. ${./.} inserts the directory
+    # of the checked-out Git folder containing this template.
+    cp -rf ${./.} "$out"
+
+    # Set some permissions
+    chmod -R +w "$out"
+
+    # Remove the template files themselves and any connection to the template's
+    # Git repository
+    rm -rf "$out/.git" "$out/idx-template".{nix,json}
   '';
 }
